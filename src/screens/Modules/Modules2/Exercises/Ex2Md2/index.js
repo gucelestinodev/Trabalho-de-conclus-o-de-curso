2cda27a3cfc4ae0e723a06f47f42e1e1af5b97b5
Gustavo Lima <gustavo.lima@onnitech.com.br>
2023-07-14 14:18:15 -0300

    :hammer: Atualização

--- src/screens/Modules/Modules2/Exercises/Ex2Md2/index.js
+++ src/screens/Modules/Modules2/Exercises/Ex2Md2/index.js
@@ -73,7 +73,7 @@ export default function Ex3Md1({ navigation }) {
         foundWordsCells,
         foundWords,
       };
-      await AsyncStorage.setItem("gameData1Ex2Md2", JSON.stringify(gameData));
+      await AsyncStorage.setItem("gameDataEx2Md2", JSON.stringify(gameData));
       console.log("Jogo salvo com sucesso!");
     } catch (error) {
       console.log("Erro ao salvar o jogo:", error);
